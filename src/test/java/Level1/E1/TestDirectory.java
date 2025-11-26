194e23c2fb3fd4fb4d32209a965161e3af17d3f9
crist <cristhianchulca49@gmail.com>
2025-11-26 13:54:05 +0100

    test: add testSortAlphabetically to verify files and directories are sorted correctly

--- src/test/java/Level1/E1/TestDirectory.java
+++ src/test/java/Level1/E1/TestDirectory.java
@@ -3,7 +3,6 @@ package Level1.E1;
 import java.io.File;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,17 +17,36 @@ public class TestDirectory {
     }
 
     @Test
-    void TestNotExistDirectory() {
+    void testNotExistDirectory() {
         directory = new File("Not Exist");
 
         assertThrows(IllegalArgumentException.class, () -> Directory.sortAlphabetically(directory), "Should throw NullPointerException if directory is doesnt exist");
     }
 
     @Test
-    void TestEmptyDirectory() {
+    void testEmptyDirectory() {
         directory = new File("Empty");
         directory.mkdir();
 
-        assertThrows(IllegalArgumentException.class, () -> Directory.sortAlphabetically(directory), "Should throw exception if directory empty");
+        assertThrows(IllegalArgumentException.class, () -> Directory.sortAlphabetically(directory), "Should throw exception if directory is empty");
+    }
+
+    @Test
+    void testSortAlphabetically() {
+        directory = new File("User");
+        directory.mkdir();
+        new File(directory, "Music").mkdir();
+        new File(directory, "Photos").mkdir();
+        try {
+            new File(directory, "Audio2.mp3").createNewFile();
+            new File(directory, "Password.txt").createNewFile();
+        } catch (Exception e) {
+            System.out.println("Error: test files no created");
+        }
+
+        String[] expected = {"Audio2.mp3", "Music", "Password.txt", "Photos"};
+        String[] result = Directory.sortAlphabetically(directory);
+
+        assertArrayEquals(expected, result);
     }
 }
