e60987da5a30ffa4c0db77e2158b15582d136a63
crist <cristhianchulca49@gmail.com>
2025-11-26 13:47:10 +0100

    feat: add TestDirectory

---
+++ src/test/java/Level1/E1/TestDirectory.java
@@ -0,0 +1,19 @@
+package Level1.E1;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestDirectory {
+    File directory;
+
+    @AfterEach
+    void tearDown() {
+        directory.delete();
+    }
+}
