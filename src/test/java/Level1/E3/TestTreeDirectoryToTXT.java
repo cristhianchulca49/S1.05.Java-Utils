4265d56bd6dae8c826e2cbed9496f51a47846b55
crist <cristhianchulca49@gmail.com>
2025-11-27 13:51:33 +0100

    test: add TestTreeDirectoryToTXT setup and teardown for directory tree tests

---
+++ src/test/java/Level1/E3/TestTreeDirectoryToTXT.java
@@ -0,0 +1,48 @@
+package Level1.E3;
+
+import org.junit.jupiter.api.*;
+import java.io.*;
+
+class TestTreeDirectoryToTXT {
+
+    private File rootDir;
+    private File outputFile;
+
+    @BeforeEach
+    void setUp() throws IOException {
+
+        rootDir = new File("TestRoot");
+        rootDir.mkdir();
+
+        File alpha = new File(rootDir, "Alpha");
+        File sub = new File(alpha, "Sub");
+        File zeta = new File(rootDir, "Zeta");
+
+        sub.mkdirs();
+        zeta.mkdir();
+
+        new File(rootDir, "music.mp3").createNewFile();
+        new File(sub, "notes.txt").createNewFile();
+        new File(sub, "aaa.log").createNewFile();
+
+
+        outputFile = new File("tree_output.txt");
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteRecursively(rootDir);
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+    }
+
+    private void deleteRecursively(File file) {
+        if (file.isDirectory()) {
+            for (File child : file.listFiles()) {
+                deleteRecursively(child);
+            }
+        }
+        file.delete();
+    }
+}
