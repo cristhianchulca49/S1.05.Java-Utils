eb1e07379df456b70797e3c3f9d0985282326dad
crist <cristhianchulca49@gmail.com>
2025-11-27 12:50:55 +0100

    feat: add @AfterEach to remove test directories after each test

---
+++ src/test/java/Level1/E2/TestTreeDirectory.java
@@ -0,0 +1,27 @@
+package Level1.E2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestTreeDirectory {
+    File root;
+
+    @AfterEach
+
+    void tearDown() {
+        deleteRecursively(root);
+    }
+
+    private void deleteRecursively(File file) {
+        if (file.isDirectory()) {
+            for (File child : file.listFiles()) {
+                deleteRecursively(child);
+            }
+        }
+        file.delete();
+    }
+}
+
