09a93f864d2263ec05936677c0b23a52a9b1c1d8
crist <cristhianchulca49@gmail.com>
2025-12-01 10:35:57 +0100

    test: add assertEquals to testing readTXTFile method

--- src/test/java/Level1/E4/TestReadTXT.java
+++ src/test/java/Level1/E4/TestReadTXT.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.io.TempDir;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Files;
-
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestReadTXT {
 
@@ -15,6 +16,10 @@ public class TestReadTXT {
     void testReadTXT(@TempDir Path tempDir) throws IOException {
         Path filePath = tempDir.resolve("Test.txt");
         Files.writeString(filePath, "First Line\nSecond Line");
+        List <String> result = ReadTXT.readTxtFile(filePath);
 
+        assertEquals(2, result.size());
+        assertEquals("First Line", result.get(0));
+        assertEquals("Second Line", result.get(1));
     }
 }
