0402fefe3ceb8ca36f2c02f19625c3917fe6bf0b
crist <cristhianchulca49@gmail.com>
2025-12-01 11:20:32 +0100

    feat: add sortAlphabetically() with Files.walk and case-insensitive Comparator

--- src/main/java/Level1/E5/DirectorySerialized.java
+++ src/main/java/Level1/E5/DirectorySerialized.java
@@ -3,10 +3,20 @@ package Level1.E5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 
 public class DirectorySerialized {
-    private static void validateDirectory(Path directory) throws IOException {
+    private static void sortAlphabetically(Path directory) throws IOException {
+        try (Stream<Path> filesDirectory = Files.walk(directory)) {
+            filesDirectory
+                    .sorted(Comparator.comparing(file -> file.getFileName().toString(), String.CASE_INSENSITIVE_ORDER))
+                    .toList();
+        }
+    }
+
+    private static void validateDirectory(Path directory) {
         if (Files.notExists(directory) || !Files.isDirectory(directory)) {
             throw new IllegalArgumentException("Directory does not valid");
         }
