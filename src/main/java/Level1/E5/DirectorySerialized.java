faf2fd2be3b82a396d62fa054c2382a310d1706c
crist <cristhianchulca49@gmail.com>
2025-12-01 10:55:45 +0100

    test: add validateDirectory() tests to verify valid non-existing directories

--- src/main/java/Level1/E5/DirectorySerialized.java
+++ src/main/java/Level1/E5/DirectorySerialized.java
@@ -1,5 +1,14 @@
 package Level1.E5;
 
-public class DirectorySerialized {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+
+public class DirectorySerialized {
+    private static void validateDirectory(Path directory) throws IOException {
+        if (Files.notExists(directory) || !Files.isDirectory(directory)) {
+            throw new IllegalArgumentException("Directory does not valid");
+        }
+    }
 }
