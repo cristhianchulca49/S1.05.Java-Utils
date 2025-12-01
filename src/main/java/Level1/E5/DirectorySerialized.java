2eb54b4e9542789b4bc2c30844323da3a4435c72
crist <cristhianchulca49@gmail.com>
2025-12-01 11:46:46 +0100

    feat: add writeTXTFile() with Files.newBufferedWriter to write name, type, and lastModified in a file.TXT

--- src/main/java/Level1/E5/DirectorySerialized.java
+++ src/main/java/Level1/E5/DirectorySerialized.java
@@ -1,9 +1,11 @@
 package Level1.E5;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Stream;
 
 
@@ -13,6 +15,23 @@ public class DirectorySerialized {
             filesDirectory
                     .sorted(Comparator.comparing(file -> file.getFileName().toString(), String.CASE_INSENSITIVE_ORDER))
                     .toList();
+        }
+    }
+
+    private static void writeTXTFile(List<Path> directory, Path pathNameTXTFile) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(pathNameTXTFile)) {
+            for (Path path : directory) {
+                String type = Files.isDirectory(path) ? "D" : "F";
+                String name = path.getFileName().toString();
+                String lastModified = Files.getLastModifiedTime(path)
+                        .toInstant()
+                        .toString()
+                        .replace("T", " ")
+                        .substring(0, 19);
+
+                writer.write(type + " - " + name + " | " + lastModified);
+                writer.newLine();
+            }
         }
     }
 
