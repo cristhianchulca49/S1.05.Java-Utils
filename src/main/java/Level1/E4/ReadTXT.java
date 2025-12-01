5b7717850742a949541a85d98923c0c0527caddb
crist <cristhianchulca49@gmail.com>
2025-12-01 10:32:26 +0100

    feat: add readTXTFile method to display file content and return List<String>

--- src/main/java/Level1/E4/ReadTXT.java
+++ src/main/java/Level1/E4/ReadTXT.java
@@ -1,5 +1,14 @@
 package Level1.E4;
 
-public class ReadTXT {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+public class ReadTXT {
+    public static List<String> readTxtFile(Path path) throws IOException {
+        List<String> fileRead= Files.readAllLines(path);
+        fileRead.forEach(System.out::println);
+        return fileRead;
+    }
 }
